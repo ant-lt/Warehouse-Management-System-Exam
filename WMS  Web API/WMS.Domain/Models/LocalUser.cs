b2f070250ed6fa6e3724a89f0366b58da0773637
Vitas Gruzdas <v.gruzdas74@gmail.com>
2023-01-05 21:22:39 +0200

    Users management and control tables

---
+++ WMS  Web API/WMS.Domain/Models/LocalUser.cs
@@ -0,0 +1,16 @@
+﻿namespace WMS.Domain.Models
+{
+    public class LocalUser
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool Active { get; set; }
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; } 
+  
+    }
+}
